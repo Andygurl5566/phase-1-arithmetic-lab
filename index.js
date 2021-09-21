8a4b960535735b4966b9e55d0ee70e9fcf88bc46
Andrea Freeman <afreeman5566@gmail.com>
2021-09-21 03:53:11 -0500

    commit tree bafecde3e5786b8bdaa4f7cae45a1d4177877eab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+let num1 = 2
+
+let num2 = 31
+
+let multiply = num1 * num2
+
+// Return an Integer between 1 and 11
+let random = Math.floor(Math.random()*11)
+
+
+// returns the remainder of 14 * 10
+let num3 = 14
+let num4 = 10
+
+let mod = num3 % num4
+
+//
+
+let max = Math.max(13, 15, 20)
+
